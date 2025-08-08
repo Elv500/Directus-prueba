b370c7e192629743ebae328acf496a0a3ac220c7
Elv500 <elvis.alvarez.2001@gmail.com>
2025-08-07 21:02:19 -0400

    Primer test automatizado

---
+++ tests/test_prueba.py
@@ -0,0 +1,17 @@
+import pytest
+import requests
+import json
+
+def test_login_exitoso():
+    url = "http://localhost:8055/auth/login"
+
+    payload = json.dumps({
+    "email": "admin@example.com",
+    "password": "d1r3ctu5"
+    })
+    headers = {
+    'Content-Type': 'application/json'
+    }
+    response = requests.request("POST", url, headers=headers, data=payload)
+    
+    assert response.status_code == 200
